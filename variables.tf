2188df59bd3fd91f84ed925b92c1967a075d782b
Edgar Kalaev <edka@mail.ru>
2022-10-16 18:58:02 +0300

    CHANGE: ADD variables description

--- variables.tf
+++ variables.tf
@@ -1,20 +1,24 @@
 variable "project_name" {
-  type    = string
-  default = "ha-vpn"
+  type        = string
+  default     = "ha-vpn"
+  description = "Some common prefix"
 }
 
 variable "region" {
-  type    = string
-  default = "us-west-2"
+  type        = string
+  default     = "us-west-2"
+  description = "Region you'll pretend to work from"
 }
 
 variable "instance_type" {
-  type    = string
-  default = "t3.micro"
+  type        = string
+  default     = "t3.micro"
+  description = "Instance type. Refer to ami_search, add your own if necessary."
 }
 
 variable "ami_search" {
-  type = map(object({ ami_owner = string, ami_filter = string }))
+  type        = map(object({ ami_owner = string, ami_filter = string }))
+  description = "Mappings for selected instance types to narrow AMI search"
   default = {
     "t2.micro" = {
       ami_owner  = "099720109477"
@@ -40,21 +44,25 @@ variable "ami_search" {
 }
 
 variable "vpc_cidr_block" {
-  type    = string
-  default = "10.10.0.0/16"
+  type        = string
+  default     = "10.10.0.0/16"
+  description = "CIDR block for newly-created VPC"
 }
 
 variable "dns_zone" {
-  type    = string
-  default = "example.com"
+  type        = string
+  default     = "example.com"
+  description = "DNS zone to manipulate. Must be hosted in your Route53"
 }
 
 variable "instance_dns_name" {
-  type    = string
-  default = "vpn"
+  type        = string
+  default     = "vpn"
+  description = "Instance DNS record within your zone"
 }
 
 variable "pub_key_path" {
-  type    = string
-  default = "~/.ssh/id_rsa.pub"
+  type        = string
+  default     = "~/.ssh/id_rsa.pub"
+  description = "Public key to provision to the instance"
 }
